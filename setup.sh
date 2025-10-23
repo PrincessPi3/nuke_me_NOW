f2fe96b74ca6f4499a5d4e415d1fc3165d8681dc
PrincessPi3 <human.bagel@gmail.com>
2025-10-23 01:40:17 -0600

    1761205218

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 # todo: check for needed packages and install them
 # todo: setup keyboard and terminal shortcuts
 # compile the wrapper
-gcc wrapper.c -o wrapper
+# gcc wrapper.c -o wrapper
+gcc nuke_me_NOW.c -o nuke_me_NOW
 
 # change user and group for wrapper (for suid)
-sudo chown root:root wrapper
+sudo chown root:root nuke_me_NOW
 
 # make scripts executable
-chmod +x *.sh
+# chmod +x *.sh
 # sudo chown root:root nuke_me_NOW.sh
 
 # set suid
-sudo chmod 4755 wrapper
+sudo chmod 4755 nuke_me_NOW
 
 # sudo chmod 4777 nuke_me_NOW.sh
 # sudo chmod 4777 $(which cryptsetup)

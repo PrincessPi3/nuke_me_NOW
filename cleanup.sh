d3cc07614de16d10c439ecbd4093d1950e28bc0b
PrincessPi3 <human.bagel@gmail.com>
2025-10-23 01:21:28 -0600

    1761204089

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 rm -f *.o 2>/dev/null # object files
 rm -f *.out 2>/dev/null
+
+# built binaries
 rm poweroff_aarch64 2>/dev/null
 rm -f build_poweroff_x86_x64 2>/dev/null
+rm -f wrapper 2>/dev/null

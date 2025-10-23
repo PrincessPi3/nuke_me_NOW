d0eaeffa9e2dd0133ad1861b455ac6e33e437d4d
PrincessPi3 <human.bagel@gmail.com>
2025-10-23 02:54:09 -0600

    1761209649

--- nuke_me_NOW.c
+++ nuke_me_NOW.c
@@ -10,8 +10,8 @@ int main(void) {
     // note: try and figure out the damned proper devices L:"awzasaa"
 #ifdef DEBUG
     system("whoami");
-    system("sudo whoami");
-    system("cryptsetup erase -q");
+    // system("sudo whoami");
+    system("cryptsetup erase -q CRYPT_DISK");
     system("poweroff -ff");
 #else
     system("cryptsetup erase -q CRYPT_DISK 2>>DEVNULL 1>DEVNULL");

af049b7e5133cc941efe4d2581961b2b20d1059c
PrincessPi3 <human.bagel@gmail.com>
2025-10-23 03:07:33 -0600

    1761210453

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 
 # compile the wrapper
 # gcc wrapper.c -o wrapper
-gcc nuke_me_NOW.c -o nuke_me_NOW
+sudo gcc nuke_me_NOW.c -o nuke_me_NOW
 
 # change user and group for wrapper (for suid)
 sudo chown root:root nuke_me_NOW

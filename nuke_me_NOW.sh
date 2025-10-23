329eefa6acc6b8039c3277725055c4f77a5cc2af
PrincessPi3 <human.bagel@gmail.com>
2025-10-23 00:54:59 -0600

    1761202499

--- nuke_me_NOW.sh
+++ nuke_me_NOW.sh
@@ -14,7 +14,6 @@ fi
 lsblk --list | awk '{printf "%s%s\n", "/dev/",$1}' | tail -n +2 | \
     while read device; do
         echo $device
-        cryptsetup isLuks $device
         if [[ $(cryptsetup isLuks $device) == 0 ]]; then
             # nuke the luks headers
             nice -20 cryptsetup erase -q $device 1>>$output_log 2>>$error_log || cryptsetup erase -q --disable-lock $device 1>>$output_log 2>>$error_log

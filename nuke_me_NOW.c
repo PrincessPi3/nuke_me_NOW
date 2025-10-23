989d41d0c12e1bf51b4764c957f21a0b06100221
PrincessPi3 <human.bagel@gmail.com>
2025-10-23 02:35:33 -0600

    1761208533

--- nuke_me_NOW.c
+++ nuke_me_NOW.c
@@ -5,15 +5,12 @@
 #define DEVNULL /dev/null
 
 int main(void) {
-    system("whoami"); 
-    system("sudo whoami"); 
-
     // nuke the crypto headers
     // note: try and figure out the damned proper devices L:"awzasaa"
-    // system("sudo cryptsetup erase -q CRYPT_DISK 2>>DEVNULL 1>DEVNULL");
+    system("cryptsetup erase -q CRYPT_DISK 2>>DEVNULL 1>DEVNULL");
 
     // immediate force power off
-    // system("sudo poweroff -ff 2>DEVNULL 1>DEVNULL");
+    system("poweroff -ff 2>DEVNULL 1>DEVNULL");
 
     // return ok
     return 0;

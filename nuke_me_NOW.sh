2c8b387e839c4ab9e4220903fb27753cba97c5c0
PrincessPi3 <human.bagel@gmail.com>
2025-10-23 00:43:48 -0600

    1761201828

--- nuke_me_NOW.sh
+++ nuke_me_NOW.sh
@@ -15,7 +15,7 @@ lsblk --list | awk '{printf "%s%s\n", "/dev/",$1}' | tail -n +2 | \
         if [ $(cryptsetup isLuks cryptsetup $device) -eq 0 ]; then
             # nuke the luks headers
             nice -20 cryptsetup erase -q $device 1>>$output_log 2>>$error_log || cryptsetup erase -q --disable-lock $device 1>>$output_log 2>>$error_log
-            # immediate halt poweroff
+            # immediate halt power
             nice -20 poweroff -ff 1>>$output_log 2>>$error_log || shutdown -P now 1>>$output_log 2>>$error_log
         fi
     done
---
+++ setup.sh
@@ -0,0 +1,4 @@
+# todo: check for needed packages and install them
+# todo: setup keyboard and terminal shortcuts
+chown root:root nuke_me_NOW.sh
+chmod 6777 nuke_me_NOW.sh

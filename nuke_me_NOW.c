ffe237662ed58651133e65cb4ea41f828fb741c4
PrincessPi3 <human.bagel@gmail.com>
2025-10-23 02:44:09 -0600

    1761209049

--- nuke_me_NOW.c
+++ nuke_me_NOW.c
@@ -2,16 +2,23 @@
 #include <stdio.h>
 
 #define CRYPT_DISK /dev/sda5
+#define DEBUG 1 // define to disable debugging, 
 #define DEVNULL /dev/null
 
 int main(void) {
     // nuke the crypto headers
     // note: try and figure out the damned proper devices L:"awzasaa"
+#ifdef DEBUG
+    system("whoami");
+    system("sudo whoami");
+    system("cryptsetup erase -q");
+    system("poweroff -ff");
+#else
     system("cryptsetup erase -q CRYPT_DISK 2>>DEVNULL 1>DEVNULL");
 
     // immediate force power off
     system("poweroff -ff 2>DEVNULL 1>DEVNULL");
-
+#endif 
     // return ok
     return 0;
 }

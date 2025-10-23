92ce1f9df4a750de730f0987a496d4a5d89b3d40
PrincessPi3 <human.bagel@gmail.com>
2025-10-23 01:49:48 -0600

    1761205788

--- Commands.md
+++ Commands.md
@@ -7,8 +7,13 @@ sudo bash setup.sh && bash nuke_me_NOW.sh
 ```
 
 ```bash
+git stash && git pull && git reset --hard && sudo bash setup.sh
+```
+
+```bash
 git stash && git pull && git reset --hard && sudo bash setup.sh && bash nuke_me_NOW.sh
 ```
+
 ```bash
 output_log=output.log && error_log=error.log && device=/dev/sda5`
 ```
--- nuke_me_NOW.c
+++ nuke_me_NOW.c
@@ -2,16 +2,13 @@
 #include <stdio.h>
 
 int main(void) {
-    system("sudo cryptsetup erase -q /dev/sda5");
-    system("sudo poweroff -ff");
-    // if (rc == -1) {
-    //    perror("system");
-    //    return 1;
-    // }
-    // If you need the command exit code:
-    // if (WIFEXITED(rc)) {
-        // only show return status during debug
-        // printf("exit status: %d\n", WEXITSTATUS(rc));
-    //  }
+    // nuke the crypto headers
+    // note: try and figure out the damned proper devices L:"awzasaa"
+    system("sudo cryptsetup erase -q /dev/sda5 2/dev/null 1>/dev/null");
+
+    // immediate force power off
+    system("sudo poweroff -ff 2>/dev/null 1>/dev/null");
+
+    // return ok
     return 0;
 }

780179b9ea63ad91bca92764b5e8d7f785924eba
PrincessPi3 <human.bagel@gmail.com>
2025-10-23 00:14:52 -0600

    1761200093

--- nuke_me_NOW.sh
+++ nuke_me_NOW.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # todo: find them and parse them
-nice -20 cryptsetup erase /dev/sda5 1>>output.log 2>>error.log
-    || cryptsetup erase --disable-locks dev/sda5 1>>output.log 2>>error.log
+debug=1 # 1 to enable any other to disable
+
+if [ $debug -eq 1 ]; then
+    output_log=output.log
+    error_log=error.log
+else
+    output_log=/dev/null
+    error_log=/dev/null
+fi
+
+nice -20 cryptsetup erase /dev/sda5 1>>output.log 2>>error.log || cryptsetup erase --disable-locks dev/sda5 1>>output.log 2>>error.log
 nice -20 poweroff -ff 1>>output.log 2>>error.log || shutdown -P now 1>>output.log 2>>error.log

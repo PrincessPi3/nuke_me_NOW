c300b40e705bbe871d02c8dccb952e947b1118af
PrincessPi3 <human.bagel@gmail.com>
2025-10-23 01:00:05 -0600

    1761202805

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # todo: check for needed packages and install them
 # todo: setup keyboard and terminal shortcuts
 chown root:root nuke_me_NOW.sh
-chmod 4755 nuke_me_NOW.sh
+chmod 4777 nuke_me_NOW.sh
